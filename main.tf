1a3714a762e45794ce5d1a0fd51ac6013f237f0d
Devin Lewis <devin.lewis@ihawkcs.com>
2022-04-04 08:34:19 -0500

    Hardened security groups and ports required for establishing the SageMaker & Glue Dev Endpoint connection.

--- main.tf
+++ main.tf
@@ -19,39 +19,33 @@ provider "aws" {
 ### SageMaker Security Group
 ###############################################################################
 resource "aws_security_group" "smNotebook" {
-  name        = "DJL-SM-NOTEBOOK-SG"
+  name        = "TF-SM-NOTEBOOK-SG"
   description = "Security group for SageMaker Notebook"
   vpc_id      = local.vpc-id
 
-  ingress {
-    description      = "Allow Local traffic from VPC"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["10.0.0.0/16"]
-  }
-
-  egress {
-    description      = "Outbound allow"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
   tags = {
-    Name = "DJL-SM-NOTEBOOK-SG"
+    Name = "TF-SM-NOTEBOOK-SG"
   }
 }
 
 resource "aws_security_group_rule" "glue-dev-endpoint-rule" {
-  type              = "ingress"
-  description       = "Allow traffic from the Glue Dev Endpoint"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
+  type              = "egress"
+  description       = "Allow outbound SSH traffic to the Glue Dev Endpoint SG"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.smNotebook.id}"
   source_security_group_id = "${aws_security_group.devEndpoint.id}"
+}
+
+resource "aws_security_group_rule" "smnb-allow-https-out" {
+  type              = "egress"
+  description       = "Allow outbound HTTPS traffic to the internet (At minimum needed for access to the S3 bucket ws-glue-jes-prod-us-east-1-assets)"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.smNotebook.id}"
 }
 ###############################################################################
 
@@ -65,8 +59,8 @@ resource "aws_sagemaker_notebook_instance" "ni" {
   instance_type = "ml.t2.medium"
   platform_identifier = "notebook-al1-v1"
   subnet_id           = local.subnet-id
-  security_groups     = [aws_security_group.smNotebook.id, aws_security_group.devEndpoint.id]
-  direct_internet_access = "Enabled"
+  security_groups     = [aws_security_group.smNotebook.id]
+  direct_internet_access = "Disabled"
   lifecycle_config_name  = aws_sagemaker_notebook_instance_lifecycle_configuration.lc.name
 
   tags = {
@@ -89,37 +83,31 @@ resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "lc" {
 ### Glue Developer Endpoint Security Group
 ###############################################################################
 resource "aws_security_group" "devEndpoint" {
-  name        = "DJL-GLUE-SG"
+  name        = "TF-GLUE-SG"
   description = "Allow traffic local VPC"
   vpc_id      = local.vpc-id
 
-  ingress {
-    description      = "Allow all traffic from local VPC"
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["10.0.0.0/16"]
-  }
-
-  egress {
-    description      = "Outbound allow"
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
   tags = {
-    Name = "DJL-GLUE-SG"
+    Name = "TF-GLUE-SG"
   }
 }
 
-resource "aws_security_group_rule" "self-referencing-rule" {
+resource "aws_security_group_rule" "self-referencing-ingress-rule" {
   type              = "ingress"
-  description       = "Sefl-referencing ingress rule"
+  description       = "Self-referencing ingress rule"
   from_port         = 0
-  to_port           = 0
-  protocol          = -1
+  to_port           = 65535
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.devEndpoint.id}"
+  source_security_group_id = "${aws_security_group.devEndpoint.id}"
+}
+
+resource "aws_security_group_rule" "self-referencing-egress-rule" {
+  type              = "egress"
+  description       = "Self-referencing egress rule"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.devEndpoint.id}"
   source_security_group_id = "${aws_security_group.devEndpoint.id}"
 }
@@ -127,9 +115,9 @@ resource "aws_security_group_rule" "self-referencing-rule" {
 resource "aws_security_group_rule" "sagemaker-notebook-rule" {
   type              = "ingress"
   description       = "Allow traffic from the SageMaker Notebook"
-  from_port         = 0
-  to_port           = 0
-  protocol          = -1
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
   security_group_id = "${aws_security_group.devEndpoint.id}"
   source_security_group_id = "${aws_security_group.smNotebook.id}"
 }
@@ -142,13 +130,14 @@ resource "aws_security_group_rule" "sagemaker-notebook-rule" {
 resource "aws_glue_dev_endpoint" "example" {
   name     = "MyFirstDevEndpoint"
   role_arn = local.iam_role_arn
-  #public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCBQt1cdwwVeAK7N/zKgHRpI0oZw+VM/WiEhSM3g+/7uLVuVlOLUw6PBsmr7ILNhX+7r5/myWO3OZUmackN72r/L8Yw2mksdH9f+4f8h1d37yFbTmC2uH1pbmRPn1IWhK15hIwwI5U8DpuGUg62yaUWKhZoA52VSNIodPnele+utG8O7Xz20r44kpz5kW5zU4PS6TE868tSokgMGQFbTtn2RCdLj/Gqa+iUlWxnZeCBzc+22XkDVWk4Djwtga0u7OKXJwX7uDKHM2LDZXeBibs/45wYVvLrGEdhRgltlm7IQZ6/Ora311lmrALMT98yL+2eCw4ibR3qWF7HN+2VPlLz DemoVPC_Key_Pair"
   security_group_ids = [aws_security_group.devEndpoint.id]
   subnet_id = local.subnet-id
   glue_version = "1.0"
   arguments =  tomap({"--enable-glue-datacatalog"=" ", "GLUE_PYTHON_VERSION"="3"})
 
-  depends_on = [aws_security_group.devEndpoint, aws_security_group_rule.self-referencing-rule]
+  depends_on = [aws_security_group.devEndpoint, 
+                aws_security_group_rule.self-referencing-ingress-rule,
+                aws_security_group_rule.self-referencing-egress-rule]
 }
 ###############################################################################
 
